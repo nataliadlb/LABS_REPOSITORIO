4c1baeed4cc36b585a24dafa745e8e4afd5da7de
nataliadlb <dele18193@uvg.edu.gt>
2021-01-25 12:44:34 -0600

    Se corrigió esquemático (YA ESTA BIEN))

---
+++ Lab1_Digital2.X/Main_Lab1_Digital2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Main_Lab1_Digital2.c
+ * Author: Nataliadlb
+ *
+ * Created on 25 de enero de 2021, 10:08 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
--- Lab1_Digital2.X/nbproject/Makefile-default.mk
+++ Lab1_Digital2.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=Main_Lab1_Digital2.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Main_Lab1_Digital2.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/Main_Lab1_Digital2.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/Main_Lab1_Digital2.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=Main_Lab1_Digital2.c
 
 
 
@@ -94,7 +94,23 @@ MP_PROCESSOR_OPTION=16F887
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/Main_Lab1_Digital2.p1: Main_Lab1_Digital2.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/Main_Lab1_Digital2.p1.d 
+	@${RM} ${OBJECTDIR}/Main_Lab1_Digital2.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Main_Lab1_Digital2.p1 Main_Lab1_Digital2.c 
+	@-${MV} ${OBJECTDIR}/Main_Lab1_Digital2.d ${OBJECTDIR}/Main_Lab1_Digital2.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Main_Lab1_Digital2.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/Main_Lab1_Digital2.p1: Main_Lab1_Digital2.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/Main_Lab1_Digital2.p1.d 
+	@${RM} ${OBJECTDIR}/Main_Lab1_Digital2.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Main_Lab1_Digital2.p1 Main_Lab1_Digital2.c 
+	@-${MV} ${OBJECTDIR}/Main_Lab1_Digital2.d ${OBJECTDIR}/Main_Lab1_Digital2.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Main_Lab1_Digital2.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
