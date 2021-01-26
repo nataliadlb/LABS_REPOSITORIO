a4e6f50c7e1f1429fff080497a60c495666f58ab
nataliadlb <dele18193@uvg.edu.gt>
2021-01-25 19:00:45 -0600

    Se creó nuevo archivo de programación
    Prueba para ver si funcionaba el Template

--- LAB_1-DIGITAL 2/Lab1_progra_digital2.X/nbproject/Makefile-impl.mk
+++ LAB_1-DIGITAL 2/Lab1_progra_digital2.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Lab1_Digital2.X
+PROJECTNAME=Lab1_progra_digital2.X
 
 # Active Configuration
 DEFAULTCONF=default
