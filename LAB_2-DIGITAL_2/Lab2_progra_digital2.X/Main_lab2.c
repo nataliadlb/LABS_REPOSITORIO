4790975071903576a1f5bc68f70c417de3bc4fca
nataliadlb <dele18193@uvg.edu.gt>
2021-01-29 17:28:55 -0600

    Se creó archivo de MPLAB X del lab 2

---
+++ LAB_2-DIGITAL_2/Lab2_progra_digital2.X/Main_lab2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Main_lab2.c
+ * Author: Nataliadlb
+ *
+ * Created on 29 de enero de 2021, 05:28 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ LAB_2-DIGITAL_2/Lab2_progra_digital2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Lab2_progra_digital2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lab2_progra_digital2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lab2progradigital2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab2progradigital2.x.tar
