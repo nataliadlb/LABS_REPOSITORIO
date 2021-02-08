938895acb9889a9eed8fff8b47b34022b493d9fa
nataliadlb <dele18193@uvg.edu.gt>
2021-02-07 23:58:32 -0600

    Revert "%%%%%%% LAB # 3 %%%%%%%"
    This reverts commit 2010f2f0da4530ae62b87a65c9e6a90c3276ddac.

--- LAB3-DIGITAL2.X/Oscilador.h
+++ LAB3-DIGITAL2.X/Oscilador.h
@@ -5,8 +5,8 @@
  * Revision history: 
  */
 
-
-
+// This is a guard condition so that contents of this file are not included
+// more than once.  
 #ifndef OSCILADOR_H
 #define	OSCILADOR_H
 
--- LAB3-DIGITAL2.X/build/default/production/pseudocodigo_lab3.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/pseudocodigo_lab3.p1:  \
-pseudocodigo_lab3.c  \
-Oscilador.h 
