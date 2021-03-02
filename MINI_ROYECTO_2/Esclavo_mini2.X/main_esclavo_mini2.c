b2cdbf8b59a0e906b3c06f3b6e43fff469e76c2b
nataliadlb <dele18193@uvg.edu.gt>
2021-03-02 14:17:30 -0600

    Se agregó librería al maestro I2C

---
+++ MINI_ROYECTO_2/Esclavo_mini2.X/main_esclavo_mini2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main_esclavo_mini2.c
+ * Author: Nataliadlb
+ *
+ * Created on 2 de marzo de 2021, 02:10 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ MINI_ROYECTO_2/Esclavo_mini2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Esclavo_mini2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Esclavo_mini2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=esclavomini2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/esclavomini2.x.tar
