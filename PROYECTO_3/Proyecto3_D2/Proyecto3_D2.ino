4856d51a1f5b8391d374084b29612d02f7ab5f75
nataliadlb <dele18193@uvg.edu.gt>
2021-04-13 08:43:46 -0600

    Ya aparece fondo :)

--- PROYECTO_3/Proyecto3_D2/Proyecto3_D2.ino
+++ PROYECTO_3/Proyecto3_D2/Proyecto3_D2.ino
@@ -60,13 +60,13 @@ void setup() {
   LCD_Init();
   LCD_Clear(0x0000);
   
-  FillRect(0, 0, 319, 206, 0x421b);
-  String text1 = "Super Mario World!";
-  LCD_Print(text1, 20, 100, 2, 0xffff, 0x421b);
+//  FillRect(0, 0, 319, 206, 0x421b);
+//  String text1 = "Super Mario World!";
+//  LCD_Print(text1, 20, 100, 2, 0xffff, 0x421b);
 //LCD_Sprite(int x, int y, int width, int height, unsigned char bitmap[],int columns, int index, char flip, char offset);
 //    
 //  //LCD_Bitmap(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned char bitmap[]);
-//  LCD_Bitmap(0, 0, 320, 240, fondo);
+  LCD_Bitmap(0, 0, 320, 240, fondo);
   
 //  for(int x = 0; x <319; x++){
 //    LCD_Bitmap(x, 52, 16, 16, tile2);
@@ -82,52 +82,53 @@ void setup() {
 // Loop Infinito
 //***************************************************************************************************************************************
 void loop() {
-  for(int x = 0; x <320-32; x++){
-    delay(15);
-    int anim2 = (x/35)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,1);
-    V_line( x -1, 100, 24, 0x421b);
-  
-    //LCD_Bitmap(x, 100, 32, 32, prueba);
-    
-    int anim = (x/11)%8;
-    
-
-    int anim3 = (x/11)%4;
-    
-    LCD_Sprite(x, 20, 16, 32, mario,8, anim,1, 0);
-    V_line( x -1, 20, 32, 0x421b);
- 
-    //LCD_Sprite(x,100,32,32,bowser,4,anim3,0,1);
-    //V_line( x -1, 100, 32, 0x421b);
- 
- 
-    LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,1, 0);
-    V_line( x -1, 140, 16, 0x421b);
-  
-    LCD_Sprite(x, 175, 16, 32, luigi,8, anim,1, 0);
-    V_line( x -1, 175, 32, 0x421b);
-  }
-  for(int x = 320-32; x >0; x--){
-    delay(5);
-    int anim = (x/11)%8;
-    int anim2 = (x/11)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,0);
-    V_line( x + 16, 100, 24, 0x421b);
-    
-    //LCD_Bitmap(x, 100, 32, 32, prueba);
-    
-    //LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,0, 0);
-    //V_line( x + 16, 140, 16, 0x421b);
-    
-    //LCD_Sprite(x, 175, 16, 32, luigi,8, anim,0, 0);
-    //V_line( x + 16, 175, 32, 0x421b);
-
-    //LCD_Sprite(x, 20, 16, 32, mario,8, anim,0, 0);
-    //V_line( x + 16, 20, 32, 0x421b);
-  } 
+//  for(int x = 0; x <320-32; x++){
+//    delay(15);
+//    int anim2 = (x/35)%2;
+//    
+//    LCD_Sprite(x,100,16,24,planta,2,anim2,0,1);
+//    V_line( x -1, 100, 24, 0x421b);
+//  
+//    //LCD_Bitmap(x, 100, 32, 32, prueba);
+//    
+//    int anim = (x/11)%8;
+//    
+//
+//    int anim3 = (x/11)%4;
+//    
+//    LCD_Sprite(x, 20, 16, 32, mario,8, anim,1, 0);
+//    V_line( x -1, 20, 32, 0x421b);
+// 
+//    //LCD_Sprite(x,100,32,32,bowser,4,anim3,0,1);
+//    //V_line( x -1, 100, 32, 0x421b);
+// 
+// 
+//    LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,1, 0);
+//    V_line( x -1, 140, 16, 0x421b);
+//  
+//    LCD_Sprite(x, 175, 16, 32, luigi,8, anim,1, 0);
+//    V_line( x -1, 175, 32, 0x421b);
+//  }
+//  
+//  for(int x = 320-32; x >0; x--){
+//    delay(5);
+//    int anim = (x/11)%8;
+//    int anim2 = (x/11)%2;
+//    
+//    LCD_Sprite(x,100,16,24,planta,2,anim2,0,0);
+//    V_line( x + 16, 100, 24, 0x421b);
+//    
+//    //LCD_Bitmap(x, 100, 32, 32, prueba);
+//    
+//    //LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,0, 0);
+//    //V_line( x + 16, 140, 16, 0x421b);
+//    
+//    //LCD_Sprite(x, 175, 16, 32, luigi,8, anim,0, 0);
+//    //V_line( x + 16, 175, 32, 0x421b);
+//
+//    //LCD_Sprite(x, 20, 16, 32, mario,8, anim,0, 0);
+//    //V_line( x + 16, 20, 32, 0x421b);
+//  } 
 
 }
 //***************************************************************************************************************************************
