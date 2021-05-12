f168158882f89f71c2569ef80d8e9f4ffebab9d7
nataliadlb <dele18193@uvg.edu.gt>
2021-05-12 09:30:05 -0600

    Se agrego UART
    no se si funciona aun

--- LAB7-DIGITAL2/PRUEBA2/main.c
+++ LAB7-DIGITAL2/PRUEBA2/main.c
@@ -21,7 +21,9 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
+#include "driverlib/uart.h"
 
+//#define XTAL 16000000
 #define LED_ROJO GPIO_PIN_1
 #define LED_VERDE GPIO_PIN_3
 #define LED_AZUL GPIO_PIN_2
@@ -39,8 +41,8 @@ uint32_t ui32Period;
 // PROTOTIPOS DE FUNCIONES
 //
 //*****************************************************************************
-
-
+void Timer0IntHandler(void);
+void InitUART(void);
 
 //*****************************************************************************
 //
@@ -61,7 +63,6 @@ __error__(char *pcFilename, uint32_t ui32Line)
 //
 //*****************************************************************************
 
-
 int main(void){
     //Config de reloj a 40MHz
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
@@ -74,7 +75,10 @@ int main(void){
      }
     // Configurar OUTPUTS/INPUTS
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    // Se habilita el reloj para el temporizador
+
+    /**
+        CONFIGURACION TIMER
+    */
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){
     }
@@ -84,6 +88,7 @@ int main(void){
 
     // Se calcula el período para el temporizador (1 seg)
     ui32Period = (SysCtlClockGet()) / 2;
+
     // Establecer el periodo del temporizador
     TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
 
@@ -98,6 +103,14 @@ int main(void){
 
     // Se habilita el Timer
     TimerEnable(TIMER0_BASE, TIMER_A);
+
+    /**
+        CONFIGURACION UART
+    */
+    // Se inicializa la comunicación UART
+    InitUART();
+
+
 
     //*****************************************************************************
     // Loop forever.
@@ -121,13 +134,34 @@ void Timer0IntHandler(void){
 
     // Read the current state of the GPIO pin and
     // write back the opposite state
-    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_3)){
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+    if (GPIOPinRead(GPIO_PORTF_BASE, LED_AZUL)){
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_ROJO | LED_AZUL | LED_VERDE, 0);
     }
     else{
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_AZUL, LED_AZUL);
     }
     // Clear the timer interrupt
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 }
 
+//**************************************************************************************************************
+// Inicialización de UART - Tomado del ejemplo que subio Pablo Mazariegos
+//**************************************************************************************************************
+void InitUART(void){
+
+    /*Enable the GPIO Port A*/
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    /*Enable the peripheral UART Module 0*/
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    /* Make the UART pins be peripheral controlled. */
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    /* Sets the configuration of a UART. */
+    UARTConfigSetExpClk(
+            UART0_BASE, SysCtlClockGet(), 115200,
+            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    //UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+}
