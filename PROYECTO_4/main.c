56b363ebb039eb7a272046c83bbaa18bbb2bc63e
nataliadlb <dele18193@uvg.edu.gt>
2021-05-13 22:40:03 -0600

    Si se conectaron bien los leds y push

--- PROYECTO_4/main.c
+++ PROYECTO_4/main.c
@@ -27,8 +27,30 @@
 #include "grlib/grlib.h"
 #include "driverlib/pin_map.h"
 
-//#define XTAL 16000000
+#define XTAL 16000000
+#define led_green1 GPIO_PIN_5 // PORT B
+#define led_green2 GPIO_PIN_0 // PORT D
+#define led_green3 GPIO_PIN_4 // PORT E
+#define led_green4 GPIO_PIN_3 // PORT D
 
+#define led_red1 GPIO_PIN_1 // PORT B
+#define led_red2 GPIO_PIN_2 // PORT D
+#define led_red3 GPIO_PIN_4 // PORT B
+#define led_red4 GPIO_PIN_2 // PORT E
+
+#define PUSH_1 GPIO_PIN_0 // PORT B
+#define PUSH_2 GPIO_PIN_1 // PORT D
+#define PUSH_3 GPIO_PIN_5 // PORT E
+#define PUSH_4 GPIO_PIN_1 // PORT E
+
+#define led_a GPIO_PIN_7 // PORT B
+#define led_b GPIO_PIN_6 // PORT D
+#define led_c GPIO_PIN_7 // PORT C
+#define led_d GPIO_PIN_6 // PORT C
+#define led_e GPIO_PIN_5 // PORT C
+#define led_f GPIO_PIN_4 // PORT C
+#define led_g GPIO_PIN_3 // PORT B
+#define led_dp GPIO_PIN_6 // PORT B
 //*****************************************************************************
 //
 // VARIABLES
@@ -36,10 +58,6 @@
 //*****************************************************************************
 uint32_t ui32Period; //periodo para TMR0
 uint32_t ui32Status; //Status de UART
-
-uint8_t Ban_rojo = 0;
-uint8_t Ban_verde = 0;
-uint8_t Ban_azul = 0;
 char letra;
 
 //*****************************************************************************
@@ -79,78 +97,113 @@ int main(void){
     // Config reloj para habilitar puertos
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
 
-    // Verificar que se habilitó puerto F
+    // Verificar que se habilitaron los puertos
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){
      }
-    // Verificar que se habilitó puerto F
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){
      }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC)){
+         }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){
+         }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)){
+         }
 
     // Configurar OUTPUTS/INPUTS
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_4); //PUSH COMO ENTRADA
-    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //weak pull up
+    //GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
+    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_6);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_2 | GPIO_PIN_4);
+
+    GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_0); //PUSH COMO ENTRADA
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_1);
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_5);
+
+    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //weak pull DOWN
+    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //weak pull DOWN
+    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //weak pull DOWN
+    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_5, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD); //weak pull DOWN
     /**
         CONFIGURACION UART
     */
     // Se inicializa la comunicación UART
-    InitUART();
+    //InitUART();
 
-    /**
-        CONFIGURACION TIMER
-    */
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){
-    }
-
-    // Configuración del Timer 0 como temporizador períodico
-    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-
-    // Se calcula el período para el temporizador (1 seg)
-    ui32Period = (SysCtlClockGet()) / 2;
-
-    // Establecer el periodo del temporizador
-    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
-
-    TimerEnable (TIMER0_BASE, TIMER_A|TIMER_B);
-
-    // Se establece que exista la interrupción por Timeout
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-
-    //Establecemos la ISR
-    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
-
-    // Se habilita la interrupción por el TIMER0A
-    IntEnable(INT_TIMER0A);
-
-    // Se habilita el Timer
-    TimerEnable(TIMER0_BASE, TIMER_A);
-
-
-    // Se manda mensajes por UART
-    UARTCharPut(UART0_BASE, 'C');
-    UARTCharPut(UART0_BASE, 'o');
-    UARTCharPut(UART0_BASE, 'm');
-    UARTCharPut(UART0_BASE, 'e');
-    UARTCharPut(UART0_BASE, 'n');
-    UARTCharPut(UART0_BASE, 'z');
-    UARTCharPut(UART0_BASE, 'a');
-    UARTCharPut(UART0_BASE, 'm');
-    UARTCharPut(UART0_BASE, 'o');
-    UARTCharPut(UART0_BASE, 's');
-    UARTCharPut(UART0_BASE, 10);
-    UARTCharPut(UART0_BASE, 13);
-
+//    /**
+//        CONFIGURACION TIMER
+//    */
+//    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+//    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){
+//    }
+//
+//    // Configuración del Timer 0 como temporizador períodico
+//    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+//
+//    // Se calcula el período para el temporizador (1 seg)
+//    ui32Period = (SysCtlClockGet()) / 2;
+//
+//    // Establecer el periodo del temporizador
+//    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
+//
+//    TimerEnable (TIMER0_BASE, TIMER_A|TIMER_B);
+//
+//    // Se establece que exista la interrupción por Timeout
+//    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+//
+//    //Establecemos la ISR
+//    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
+//
+//    // Se habilita la interrupción por el TIMER0A
+//    IntEnable(INT_TIMER0A);
+//
+//    // Se habilita el Timer
+//    TimerEnable(TIMER0_BASE, TIMER_A);
+//
+//
+//    // Se manda mensajes por UART
+//    UARTCharPut(UART0_BASE, 'C');
+//    UARTCharPut(UART0_BASE, 'o');
+//    UARTCharPut(UART0_BASE, 'm');
+//    UARTCharPut(UART0_BASE, 'e');
+//    UARTCharPut(UART0_BASE, 'n');
+//    UARTCharPut(UART0_BASE, 'z');
+//    UARTCharPut(UART0_BASE, 'a');
+//    UARTCharPut(UART0_BASE, 'm');
+//    UARTCharPut(UART0_BASE, 'o');
+//    UARTCharPut(UART0_BASE, 's');
+//    UARTCharPut(UART0_BASE, 10);
+//    UARTCharPut(UART0_BASE, 13);
+//
 
     //*****************************************************************************
     // Loop forever.
     //*****************************************************************************
 
     while(1){
-        if((GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_4))){
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_1 , GPIO_PIN_1);
+        if((GPIOPinRead(GPIO_PORTE_BASE, PUSH_4))){
+            GPIOPinWrite(GPIO_PORTB_BASE, led_green1 , led_green1);
+            GPIOPinWrite(GPIO_PORTD_BASE, led_green2 , led_green2);
+            GPIOPinWrite(GPIO_PORTE_BASE, led_green3 , led_green3);
+            GPIOPinWrite(GPIO_PORTD_BASE, led_green4 , led_green4);
+
+            GPIOPinWrite(GPIO_PORTB_BASE, led_red1 , led_red1);
+            GPIOPinWrite(GPIO_PORTD_BASE, led_red2 , led_red2);
+            GPIOPinWrite(GPIO_PORTB_BASE, led_red3 , led_red3);
+            GPIOPinWrite(GPIO_PORTE_BASE, led_red4 , led_red4);
+
+            GPIOPinWrite(GPIO_PORTB_BASE, led_a , led_a);
+            GPIOPinWrite(GPIO_PORTD_BASE, led_b , led_b);
+            GPIOPinWrite(GPIO_PORTC_BASE, led_c , led_c);
+            GPIOPinWrite(GPIO_PORTC_BASE, led_d , led_d);
+            GPIOPinWrite(GPIO_PORTC_BASE, led_e , led_e);
+            GPIOPinWrite(GPIO_PORTC_BASE, led_f , led_f);
+            GPIOPinWrite(GPIO_PORTB_BASE, led_g , led_g);
+            GPIOPinWrite(GPIO_PORTB_BASE, led_dp , led_dp);
         }
 
     }
